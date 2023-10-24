b3b12d98e9b3c9f3554d03b0226d9b5aab5fe377
preston jones <preston.d.jones-1@ou.edu>
2023-10-24 08:36:17 -0500

    llama_index updates

--- llama_index.py
+++ llama_index.py
@@ -1,11 +1,28 @@
 import os
 import constants
 
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+from llama_index import StorageContext, load_index_from_storage
+
+
+# OpenAI API Key
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
-documents = SimpleDirectoryReader("YOUR_DATA_DIRECTORY").load_data()
+# Load the data
+documents = SimpleDirectoryReader("/Users/prestonjones/Library/CloudStorage/OneDrive-UniversityofOklahoma/cs/documentGPT/pdfs").load_data()
 index = VectorStoreIndex.from_documents(documents)
 
+# Query
 query_engine = index.as_query_engine()
 query_engine.query("what is brain cancer?")
+
+'''
+# Store data
+index.storage_context.persist()
+
+# Retrieve stored data
+# rebuild storage context
+storage_context = StorageContext.from_defaults(persist_dir='./storage')
+# load index
+index = load_index_from_storage(storage_context)
+'''
